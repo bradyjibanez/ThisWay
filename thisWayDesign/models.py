c5ecaf19268b02e2c52efc259b6494a8bc4f6b11
bradyjibanez <bradyjibanez@gmail.com>
2019-02-07 21:49:26 -0500

    and again

--- thisWayDesign/models.py
+++ thisWayDesign/models.py
@@ -6,7 +6,7 @@ from django.db import models
 	image = models.CharField(max_length=1000)
 	landmark, lat, lon = landmark(image)
 	def __str__(self):
-		return self.landmark
+		return self.landmark'''
 
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)'''
