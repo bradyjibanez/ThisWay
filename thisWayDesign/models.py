89af2fbcc5356b143a230738fcd4294ae61d14a7
bradyjibanez <bradyjibanez@gmail.com>
2019-02-07 21:50:39 -0500

    and again

--- thisWayDesign/models.py
+++ thisWayDesign/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from thisWayDesign.findLandmark import getLandmark
+#from .findLandmark import getLandmark
 
 # Create your models here.
 '''class Landmark(models.Model):
@@ -9,4 +9,4 @@ from django.db import models
 		return self.landmark'''
 
 class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)'''
+    when = models.DateTimeField("date created", auto_now_add=True)
