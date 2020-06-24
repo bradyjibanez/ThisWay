62bca0859251bad64d59de9f155a31d559f18169
Brady Ibanez <bradyjibanez@gmail.com>
2020-06-24 13:21:55 -0400

    Update settings.py

--- thisWay/settings.py
+++ thisWay/settings.py
@@ -86,7 +86,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = '3035highway7'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "thiswayfollowup@gmail.com"
 
